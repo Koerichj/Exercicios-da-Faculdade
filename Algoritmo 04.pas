5c00c48fd4e16c525f028ec541b2b1bf644bb3c9
Koerichj <130242824+Koerichj@users.noreply.github.com>
2023-04-09 10:44:51 -0300

    Add files via upload

---
+++ Algoritmo 04.pas
@@ -0,0 +1,13 @@
+Program Media ;
+
+	//Aluno: Jean Koerich
+	//Fazer um algoritmo que leia 3 notas de um aluno, calcule e escreva  a média
+
+ 	var nota1, nota2, nota3, media :real;
+ 	
+Begin
+		writeln ('Informe as tres notas do aluno a seguir:');
+		read (nota1,nota2,nota3);
+		media := (nota1+nota2+nota3)/3;
+		write ('Media do Aluno : ', media:6:2); 
+End.
---
+++ Algoritmo 07.pas
@@ -0,0 +1,18 @@
+Program Algoritmo_07 ;
+
+		{Aluno: Jean Koerich
+		Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e 
+		mostre-a expressa apenas em dias. Considere que todos os anos tem 365 dias e todos os meses 30 dias}
+		
+		var Ano,Mes,Dia, Tempo :real;
+		
+Begin
+  	write('Informe a Idade em anos: ');
+  	readln(Ano);
+  	write('Informe quantos meses: ');
+  	read(Mes);
+  	write('informe quantos dias: ');
+  	readln(Dia);
+  	Tempo := (Ano*365) + Mes*30 +Dia;
+  	write('Ela possui: ', Tempo:6:0,' dias de idade'); 
+End.                                
